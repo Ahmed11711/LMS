2401cec412a010480fe99cb3b6b9a706327bad5a
Ahmed11711 <ahmed11711@gmail.com>
2026-03-11 15:53:15 +0200

    aa

--- app/Services/SmsService/SMSMISR/SmsMisrService.php
+++ app/Services/SmsService/SMSMISR/SmsMisrService.php
@@ -21,14 +21,17 @@ class SmsMisrService
         //     "message"     => $message,
         // ]);
 
-        $response = Http::asForm()->post("https://smsmisr.com/api/SMS", [
-            "environment" => "0", // 0 للتجربة
-            "username"    => "d11beea6-3569-46ba-beed-4ace835e0a1e", // تأكد إنه الـ API Key من الموقع
+        $response = Http::asForm()->post($url, [
+            "environment" => "1", // وضع التشغيل الحقيقي
+            "username"    => "d11beea6-3569-46ba-beed-4ace835e0a1e",
             "password"    => "4b8cd6c8273cc9a911e779e2ea981e9091f77c9c8b2571c5fc347de1579b09ff",
-            "sender"      => "b611afb996655a94c8e942a823f1421de42bf8335d24ba1f84c437b2ab11ca27", // الـ Sender الخاص بالتجربة
+
+            // استخدم الـ Token الخاص بـ TIQNIA اللي ظهر عندك في الجدول
+            "sender"      => "9f2f0802ec90da7d1a2c15c1ca6b6eaf008d92adf4e5d248a79435d2b61cbf57",
+
             "mobile"      => "201094321637",
             "language"    => "2",
-            "message"     => "Test Message",
+            "message"     => "Hello from TIQNIA!",
         ]);
 
         if ($response->successful()) {
