dff3fd4f4eeb808137a3bdac0cd46f49a479da17
Ahmed11711 <ahmed11711@gmail.com>
2026-03-11 15:11:41 +0200

    dd

--- app/Services/SmsService/SMSMISR/SmsMisrService.php
+++ app/Services/SmsService/SMSMISR/SmsMisrService.php
@@ -15,7 +15,7 @@ class SmsMisrService
             "environment" => "1", // 1 for production, 0 for testing
             "username"    => "d11beea6-3569-46ba-beed-4ace835e0a1e",
             "password"    => "4b8cd6c8273cc9a911e779e2ea981e9091f77c9c8b2571c5fc347de1579b09ff",
-            "sender"      => "Informative",
+            "sender"      => "Tiqnia",
             "mobile"      => $mobile,
             "language"    => "2", //  Arabic
             "message"     => $message,
