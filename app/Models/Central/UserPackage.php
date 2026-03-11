e2b7ca6fe9d457066f6204b2d18d6fecefae7853
Ahmed11711 <ahmed11711@gmail.com>
2026-03-11 14:44:47 +0200

    aa

--- app/Models/Central/UserPackage.php
+++ app/Models/Central/UserPackage.php
@@ -8,7 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class UserPackage extends Model
 {
     //
+    protected static function boot()
+    {
+        parent::boot();
 
+        // قبل عملية الحفظ في الداتابيز مباشرة
+        static::creating(function ($userPackage) {
+            // لو الـ package_name فاضي، حط القيمة الافتراضية "test"
+            if (empty($userPackage->package_name)) {
+                $userPackage->package_name = 'test';
+            }
+        });
+    }
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
