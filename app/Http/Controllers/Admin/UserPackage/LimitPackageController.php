6fcd6254e890a588f99afadb955f43bc55be1883
Ahmed11711 <Ahmedsamir11711@gmail.com>
2026-03-29 12:46:28 +0200

    ss

--- app/Http/Controllers/Admin/UserPackage/LimitPackageController.php
+++ app/Http/Controllers/Admin/UserPackage/LimitPackageController.php
@@ -20,30 +20,5 @@ class LimitPackageController extends Controller
 
 
         return $features;
-
-        if ($features->isEmpty()) {
-            return response()->json([
-                'status' => 'success',
-                'data' => []
-            ]);
-        }
-
-        $formattedData = $features->map(function ($item) {
-            return [
-                'feature'    => $item->feature_slug,
-                'label'      => $item->display_name ?? str_replace('_', ' ', ucfirst($item->feature_key)),
-                'limit'      => (int) $item->limit_count,
-                'used'       => (int) $item->consumed,
-                'remaining'  => max(0, $item->limit_count - $item->consumed),
-                'percentage' => $item->limit_count > 0
-                    ? round(($item->consumed / $item->limit_count) * 100, 2)
-                    : 0
-            ];
-        });
-
-        return response()->json([
-            'status' => 'success',
-            'data'   => $formattedData
-        ]);
     }
 }
