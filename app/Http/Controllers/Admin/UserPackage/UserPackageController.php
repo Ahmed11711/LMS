4b748b9a79aaf3f29a8475f3d5a3438eae5c31ac
Ahmed11711 <Ahmedsamir11711@gmail.com>
2026-03-30 11:30:10 +0200

    update route

--- app/Http/Controllers/Admin/UserPackage/UserPackageController.php
+++ app/Http/Controllers/Admin/UserPackage/UserPackageController.php
@@ -29,7 +29,7 @@ class UserPackageController extends BaseController
 
     public function test(Request $request)
     {
-        return  $userId = $request->get('user_id');
+        return  $userIds = $request->get('user_id');
         $tenantPackage = UserPackage::where('user_id', 1)->first();
 
         if (!$tenantPackage) {
