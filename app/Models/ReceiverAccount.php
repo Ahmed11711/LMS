adb1dc61dd1696cb20d4d3ef74c732744e53efff
Ahmed11711 <ahmedsamir11711@gmail.com>
2026-06-15 11:59:28 +0300

    ss

--- app/Models/ReceiverAccount.php
+++ app/Models/ReceiverAccount.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Casts\StorageUrlCast;
 use Illuminate\Database\Eloquent\Model;
 
 class ReceiverAccount extends Model
 {
-    //
+    protected $casts = [
+        'logo' => StorageUrlCast::class,
+    ];
 }
