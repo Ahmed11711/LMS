188407bdbe0af4dfb4db55d2b3d3e390061e825d
Ahmed11711 <ahmedsamir11711@gmail.com>
2026-07-29 15:01:02 +0300

    template

---
+++ database/migrations/2026_07_29_115641_create_templates_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('templates', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->nullable();
+            $table->string('name');
+            $table->json('content')->nullable();
+            $table->integer('course_id')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('templates');
+    }
+};
