4e2d2f10c923fd0531a1c2156c78c453144a1464
Ahmed11711 <ahmed11711@gmail.com>
2026-09-17 16:00:06 +0300

    sss

--- app/Http/Resources/Admin/Chapter/ChapterResource.php
+++ app/Http/Resources/Admin/Chapter/ChapterResource.php
@@ -2,28 +2,12 @@
 
 namespace App\Http\Resources\Admin\Chapter;
 
-use App\Http\Resources\Admin\Lesson\LessonResource;
-
-use App\Http\Resources\Admin\OnlineSession\OnlineSessionResource;
-use App\Http\Resources\Admin\PhysicalCourseDetail\PhysicalCourseDetailResource;
 use Illuminate\Http\Resources\Json\JsonResource;
+
+use App\Http\Resources\Admin\Lesson\LessonResource;
 
 class ChapterResource extends JsonResource
 {
-    // public function toArray($request): array
-    // {
-    //     return [
-    //         'id' => $this->id,
-    //         'course_id' => $this->course_id,
-    //         'title' => $this->title,
-    //         'description' => $this->description,
-    //         'order' => $this->order,
-    //         'lessons'       => LessonResource::collection($this->whenLoaded('lessons')),
-    //         'created_at' => $this->created_at,
-    //         'updated_at' => $this->updated_at,
-    //     ];
-    // }
-
     public function toArray($request): array
     {
         return [
@@ -32,20 +16,9 @@ class ChapterResource extends JsonResource
             'title' => $this->title,
             'description' => $this->description,
             'order' => $this->order,
-            'lessons' => $this->resolveLessons(),
+            'lessons'       => LessonResource::collection($this->whenLoaded('lessons')),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
-    }
-
-    private function resolveLessons()
-    {
-        $type = $this->whenLoaded('course', fn() => $this->course->type);
-
-        return match ($type) {
-            'online'  => OnlineSessionResource::collection($this->whenLoaded('onlineSessions')),
-            'offline' => PhysicalCourseDetailResource::collection($this->whenLoaded('physicalCourseDetails')),
-            default   => LessonResource::collection($this->whenLoaded('lessons')), // recorded أو أي نوع تاني
-        };
     }
 }
