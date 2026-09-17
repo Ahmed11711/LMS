672c5ef26f9724020d931e2aca4ff118b21612fa
Ahmed11711 <ahmed11711@gmail.com>
2026-09-17 15:27:42 +0300

    ss

--- app/Http/Controllers/SuperAdmin/AcademyPackage/AcademyPacakgaeController.php
+++ app/Http/Controllers/SuperAdmin/AcademyPackage/AcademyPacakgaeController.php
@@ -41,7 +41,7 @@ class AcademyPacakgaeController extends Controller
             ->paginate($perPage);
 
         $packages->getCollection()->transform(function ($package) use ($hasReceipt) {
-            $package->receipt = $hasReceipt && $package->payment_proof
+            $package->receipt = $hasReceipt && $package->receipt
                 ? asset(ltrim($package->payment_proof, '/'))
                 : null;
 
