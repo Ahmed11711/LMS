be34a774cddf7653184cac5108d7f3400c349b52
Ahmed11711 <ahmedsamir11711@gmail.com>
2026-09-20 11:01:47 +0300

    aa

--- app/Http/Controllers/Admin/UserPackage/LimitPackageController.php
+++ app/Http/Controllers/Admin/UserPackage/LimitPackageController.php
@@ -11,42 +11,42 @@ use Illuminate\Support\Str;
 class LimitPackageController extends Controller
 {
     use ApiResponseTrait;
-    // public function getUsageSummary()
-    // {
-
-    //     $features = DB::connection('tenant')
-    //         ->table('tenant_feature_usage')
-    //         ->where('is_enabled', true)
-    //         ->get();
-
-
-    //     return $this->successResponse($features, 'List Of My Use');
-    // }
-
-
-
     public function getUsageSummary()
     {
-        $labels = DB::connection('LMS_CENTER')
-            ->table('features')
-            ->get(['key', 'title', 'label'])
-            ->keyBy('key');
 
         $features = DB::connection('tenant')
             ->table('tenant_feature_usage')
             ->where('is_enabled', true)
-            ->get()
-            ->map(function ($feature) use ($labels) {
-                $central = $labels->get($feature->feature_slug);
+            ->get();
 
-                // label ← title ← slug متحول لشكل مقروء
-                $feature->label = $central?->label
-                    ?: $central?->title
-                    ?: Str::headline($feature->feature_slug);
-
-                return $feature;
-            });
 
         return $this->successResponse($features, 'List Of My Use');
     }
+
+
+    // if want get lable feature from central database, you can use this function
+    // public function getUsageSummary()
+    // {
+    //     $labels = DB::connection('LMS_CENTER')
+    //         ->table('features')
+    //         ->get(['key', 'title', 'label'])
+    //         ->keyBy('key');
+
+    //     $features = DB::connection('tenant')
+    //         ->table('tenant_feature_usage')
+    //         ->where('is_enabled', true)
+    //         ->get()
+    //         ->map(function ($feature) use ($labels) {
+    //             $central = $labels->get($feature->feature_slug);
+
+    //             // label ← title ← slug متحول لشكل مقروء
+    //             $feature->label = $central?->label
+    //                 ?: $central?->title
+    //                 ?: Str::headline($feature->feature_slug);
+
+    //             return $feature;
+    //         });
+
+    //     return $this->successResponse($features, 'List Of My Use');
+    // }
 }
