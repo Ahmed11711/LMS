e9c26d5ae2917803132f41d3da211958ec00ff0f
Ahmed11711 <ahmedsamir11711@gmail.com>
2026-09-23 12:14:00 +0300

    each

--- app/Models/Central/User.php
+++ app/Models/Central/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable implements JWTSubject
 
     protected $connection = 'LMS_CENTER'; // Central DB
     protected $table = 'users';
-    public $searchable = ['email', 'academy_name', 'username', 'phone'];
+    public $searchable = ['email', 'name', 'username', 'phone'];
 
     // protected $fillable = ['name', 'email', 'password', 'role', 'phone'];
     protected $guarded = [];
